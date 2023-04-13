08da24435feb5f0fad7523ac05d44016ff79df5b
brenerrr <36827826+brenerrr@users.noreply.github.com>
2023-04-13 18:23:50 -0300

    Adding Favicon; Faster update and Browser Title

--- app/app.py
+++ app/app.py
@@ -151,7 +151,9 @@ app = dash.Dash(
     __name__,
     suppress_callback_exceptions=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
-    server=server  # type: ignore
+    server=server,  # type: ignore
+    title='Dashboard',
+    update_title=''
 )
 
 app.layout = html.Div(className='main', children=[
